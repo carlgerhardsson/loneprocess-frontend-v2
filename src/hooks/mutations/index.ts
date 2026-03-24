43f322584fe0dbf4591717d54da8d48a460e33c6
Carl Gerhardsson <carlgerhardsson@example.com>
2026-03-24 19:50:53 +0100

    chore: remove all CRUD and unused components

--- src/hooks/mutations/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Mutation Hooks Barrel Export
- * 
- * Centralized export for all mutation hooks with optimistic updates.
- */
-
-export { useCreateActivity } from './useCreateActivity'
-export { useUpdateActivity } from './useUpdateActivity'
-export { useDeleteActivity } from './useDeleteActivity'
