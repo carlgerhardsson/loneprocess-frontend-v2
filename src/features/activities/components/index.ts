34805470baabec7e584499e489e7530557a11a81
carlgerhardsson <carl.gerhardsson@cgi.com>
2026-03-25 12:48:58 +0100

    fix: restore all component exports in index.ts
    Add back existing component exports that were accidentally removed
    Fixes: "does not provide an export named 'ActiveFilters'" error

--- src/features/activities/components/index.ts
+++ src/features/activities/components/index.ts
@@ -1,7 +1,21 @@
 /**
  * Activities Feature - Component Exports
+ * Contains both new (Fas 5.1) and existing components
  */
 
+// Existing components (keep for backward compatibility)
+export { ActiveFilters } from './ActiveFilters'
+export { ActivityDetails } from './ActivityDetails'
+export { ActivityList } from './ActivityList'
+export { ActivityListItem } from './ActivityListItem'
+export { EmptyState } from './EmptyState'
+export { FilterPanel } from './FilterPanel'
+export { FormField } from './FormField'
+export { PriorityIndicator } from './PriorityIndicator'
+export { SearchBar } from './SearchBar'
+export { StatusBadge } from './StatusBadge'
+
+// NEW: Fas 5.1 components (expandable activities)
 export { DelstegChecklist } from './DelstegChecklist'
 export { ActivityComments } from './ActivityComments'
 export { ActivityReferences } from './ActivityReferences'
