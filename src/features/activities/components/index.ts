3dccf3750db8bbc38af5983492366703868416d5
carlgerhardsson <carl.gerhardsson@cgi.com>
2026-03-25 11:56:15 +0100

    feat(fas-5.1): add activities components export index
    Export all new expandable activity components

--- src/features/activities/components/index.ts
+++ src/features/activities/components/index.ts
@@ -1,14 +1,8 @@
 /**
- * Activities feature components - READ-ONLY VERSION
+ * Activities Feature - Component Exports
  */
 
-export { ActivityList } from './ActivityList'
-export { ActivityListItem } from './ActivityListItem'
-export { ActivityDetails } from './ActivityDetails'
-export { StatusBadge } from './StatusBadge'
-export { PriorityIndicator } from './PriorityIndicator'
-export { EmptyState } from './EmptyState'
-export { SearchBar } from './SearchBar'
-export { FilterPanel } from './FilterPanel'
-export { ActiveFilters } from './ActiveFilters'
-export { FormField } from './FormField'
+export { DelstegChecklist } from './DelstegChecklist'
+export { ActivityComments } from './ActivityComments'
+export { ActivityReferences } from './ActivityReferences'
+export { ActivityListItemExpanded } from './ActivityListItemExpanded'
